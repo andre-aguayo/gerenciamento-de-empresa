241956ab2dda4aada274bc9683d2c08d595f2e15
andre-aguayo <andre.aguayo@hotmail.com>
2021-09-04 03:18:14 -0400

    Mensagem de login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'As informaçoes inseridas sao invalidas.',
+    'password' => 'Senha incorreta.',
+    'throttle' => 'Por favor, tente novamente em alguns segundos.',
 
 ];
